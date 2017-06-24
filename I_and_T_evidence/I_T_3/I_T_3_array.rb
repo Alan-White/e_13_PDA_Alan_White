f15c65ea4f1d2847134ab47af115386e77500a21
Alan White <alanwhitey@gmail.com>
2017-06-24 08:06:24 +0100

    files renamed to suit checklist

---
+++ I_and_T_evidence/I_T_3/I_T_3_array.rb
@@ -0,0 +1,23 @@
+number_array = Array.new
+number_array.push(0,8,2,1,6,3,4,5,7,9)
+
+def what_is_second(array)
+  array[1]
+end
+
+puts what_is_second (number_array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print number_array.reverse
+# number_array.map {|x| puts x * 2}
